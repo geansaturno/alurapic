4248cb81a9d62073f2d5e27db119ef3f7a45d94e
Gean Saturno Gonçalves <cronosnaeg@gmail.com>
2017-05-15 21:58:28 -0300

    work 15-05

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,7 @@
+angular.module('alurapic').controller('FotosController', function($scope){
+
+    $scope.foto = {
+        url: 'https://i0.wp.com/spacenews.com.br/wp-content/uploads/2015/11/maxresdefault.jpg?resize=696%2C435',
+        title: 'Leãozinho'
+    }
+})
