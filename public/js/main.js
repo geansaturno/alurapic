df035c3cbb79c67497e764b5bb912aa01b0d1e08
Alexander Antunes Vianna <avianna@uolinc.com>
2017-05-16 19:08:38 -0300

    aula 05-06

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,22 @@
-angular.module('alurapic', ['directivasAlurapic']);
+angular.module('alurapic', ['directivasAlurapic', 'ngAnimate', 'ngRoute'])
+.config(function($routeProvider, $locationProvider){
+
+    $locationProvider.html5Mode(true);
+
+    $routeProvider.when('/fotos', {
+        templateUrl : 'parciais/principal.html',
+        controller: 'FotosController'
+    });
+
+    $routeProvider.when('/', {
+        redirectTo: '/fotos'
+    });
+
+    $routeProvider.when('/fotos/new', {
+        template: "<h1 class='text-center'>Nova Imagem</h1>"
+    });
+
+    $routeProvider.otherwise({
+        templateUrl : 'parciais/not-found.html'
+    });
+});
